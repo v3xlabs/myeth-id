8e59d826c60257ef4bcc1d3d57b1e6fec922287d
Luc <luc@lucemans.nl>
2023-08-15 23:30:12 +0000

    Introduce Function Decoding

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,14 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use ethers::abi::ParamType;
+use ethers::{
+    abi::{AbiEncode, ParamType, Token},
+    signers::LocalWallet,
+    types::{H160, U256},
+};
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use tower_http::cors::CorsLayer;
+use std::{env, net::SocketAddr, str::FromStr};
 use tracing::info;
 
 use crate::resolver::functions::ResolverFunctionCall;
@@ -40,7 +45,9 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/gateway/:sender", post(handle_ccip));
+        .route("/gateway/:sender", post(handle_ccip)).layer(
+            CorsLayer::very_permissive()
+        );
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
@@ -56,7 +63,7 @@ async fn root() -> &'static str {
 
 async fn handle_ccip(
     Path(value): Path<String>,
-    Json(payload): Json<ResolveCCIPPostPayload>,
+    Json(request_payload): Json<ResolveCCIPPostPayload>,
 ) -> (StatusCode, Json<ResolveCCIPPostResponse>) {
     // info!(payload = ?payload, path = %value, "All Endpoint");
 
@@ -75,7 +82,7 @@ async fn handle_ccip(
 
     // info!(data = ?decoded_hex, "Decoded hex");
 
-    let data = payload.data.trim_start_matches("0x9061b923"); // Multicall
+    let data = request_payload.data.trim_start_matches("0x9061b923"); // Multicall
 
     let result = ethers::abi::decode(
         &[ParamType::Bytes, ParamType::Bytes],
@@ -114,9 +121,107 @@ async fn handle_ccip(
 
     info!(call = ?call, "Function call");
 
-    let rest_of_the_data = hex::encode(rest_of_the_data);
+    // let rest_of_the_data = hex::encode(rest_of_the_data);
 
-    info!(rest_of_the_data = ?rest_of_the_data, "Rest of the data");
+    // info!(rest_of_the_data = ?rest_of_the_data, "Rest of the data");
+
+    // let rest_of_data = hex_literal::hex!(
+    //     "3b3b57ded379bcd305d2f206378661c8f0d1ca3a2c9072a898c8d23ed4406fd965264dbd"
+    // );
+
+    // 59d1d43cd379bcd305d2f206378661c8f0d1ca3a2c9072a898c8d23ed4406fd965264dbd000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000086c6f636174696f6e000000000000000000000000000000000000000000000000
+    // 3b3b57ded379bcd305d2f206378661c8f0d1ca3a2c9072a898c8d23ed4406fd965264dbd
+    // 59d1d43cd379bcd305d2f206378661c8f0d1ca3a2c9072a898c8d23ed4406fd965264dbd000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000046e616d6500000000000000000000000000000000000000000000000000000000
+    // 59d1d43cd379bcd305d2f206378661c8f0d1ca3a2c9072a898c8d23ed4406fd965264dbd0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000b6465736372697074696f6e0000000
+
+    // get the rest of data except for the first 4 bytes
+    let payload = rest_of_the_data[4..].to_vec();
+
+    if call == ResolverFunctionCall::addr {
+        info!("ADDR Call");
+
+        let result = ethers::abi::decode(&[ParamType::FixedBytes(32)], &payload).unwrap();
+        let namehash = result[0].clone().into_fixed_bytes().unwrap();
+
+        info!(namehash = ?namehash, "Namehash");
+
+        // 0x prefixed hex string containing the result data
+        // let data = "0x225f137127d9067788314bc7fcc1f36746a3c3B5";
+        // 0x000000000000000000000000000000000000000000000000000000000000006
+        //   00000000000000000000000000000000000000000000000000000000064dbfb1b00000000000000000000000000000000000000000000000000000000000000c
+        //   00000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000002
+        //   00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004
+        //   160c26e8778324cd0d3ff04f57418b79efc07f2f87085d8fd32439d748cdfd8a9164b477ef66ce48d742ae5075cee9a16342c31074d00941b3213a11006f71f7e1b00000000000000000000000000000000000000000000000000000000000000
+
+        // let to_address = data;
+        // let valid_until = 3600;
+        // let keccak_of_request_data =
+        // let keccak_of_response_data =
+
+        // Solidity Keccak256 ['bytes', 'address', 'uint64', 'bytes32', 'bytes32']
+        // 0x1900 request.to validUntil
+
+        // messageHash = solidityKeccak256(
+        //   ['bytes', 'address', 'uint64', 'bytes32', 'bytes32'],
+        //   [
+        //     '0x1900', to, validUntil, keccak256(requestData || '0x'), keccak256(responseData)
+        // ]
+        // )
+        // sig = signer.signDigest(messageHash);
+        // signature_data = hexConcat([sig.r, sig._vs])
+        // result, valid until, signature_data
+
+        let result = "0x225f137127d9067788314bc7fcc1f36746a3c3B5"
+            .as_bytes()
+            .to_vec();
+
+        let expires: u64 = 1693140299;
+        let expires: U256 = expires.into();
+
+        let payload_data_bytes = hex::decode(request_payload.data).unwrap();
+
+        let request_hash = ethers::utils::keccak256(payload_data_bytes).to_vec();
+        let result_hash = ethers::utils::keccak256(&result).to_vec();
+
+        let sender = H160::from_str(&request_payload.sender).unwrap();
+
+        let payload_hash = ethers::abi::encode_packed(
+            vec![
+                Token::Bytes(hex::decode("1900").unwrap()),
+                Token::Address(sender),
+                Token::Uint(expires),
+                Token::Bytes(request_hash),
+                Token::Bytes(result_hash),
+            ]
+            .as_slice(),
+        )
+        .unwrap();
+
+        let payload_hash = ethers::utils::keccak256(payload_hash);
+
+        let wallet = LocalWallet::from_str(env::var("PRIVATE_KEY").unwrap().as_str()).unwrap();
+        let signature = wallet.sign_hash(payload_hash.into()).unwrap();
+
+        // TODO: Figure out hexConcat, (to_string atm)
+        let signature = signature.to_string().as_bytes().to_vec();
+
+        let data = vec![
+            Token::Bytes(result),
+            Token::Uint(expires),
+            Token::Bytes(signature),
+        ];
+
+        let data = ethers::abi::encode(data.as_slice());
+
+        let data = hex::encode(data);
+        let data = format!("0x{}", data);
+
+        return (StatusCode::OK, Json(ResolveCCIPPostResponse { data }));
+    }
+
+    if call == ResolverFunctionCall::text {
+        info!("CONTENT Call");
+    }
 
     // let function_selector = String::from_utf8_lossy(function_selector.into());
 
